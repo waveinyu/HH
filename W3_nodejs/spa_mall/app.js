b985969482077e3954a91c016fd846cc8b389944
waveinyu <waveinyu@gmail.com>
2022-07-25 17:49:43 +0900

    2-6 중간 업데이트

--- W3_nodejs/spa_mall/app.js
+++ W3_nodejs/spa_mall/app.js
@@ -2,18 +2,21 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-// arrow 함수
-// req, res라는 인자에 express가 req, res 객체를 넣어준다. -> 익스프레스가 제공하는 약속
-// app.get()라는 HTTP 메서드가 이 경로("/")로 요청이 들어왔다면, 이하가 실행되는데
-// response 객체는 express.js가 넘겨주는 거고, 그 값을 아래의 "Hello World"로 보내주겠다.
+// app.use로 미들웨어를 구현할 수 있다.
+app.use((req, res, next) => {
+  console.log("미들웨어가 구현이 됐나 @.@");
+  //   next(); // next를 주석 처리 하면 그 다음 미들웨어로 넘어가지 않기 때문에, 포트로 서버가 켜졌어도 제대로 작동하진 않는다.
+  // NextFunction : 미들웨어에서 빼놓을 수 없음
+  // 위의 경우, console.log() 실행된 뒤 next()를 만나면, 바로 다음(next) 미들웨어로 이동한다.
+  // Router도 미들웨어이기 때문에 app.get으로 넘어간다.
+  res.send("미들웨어의 응답이에요");
+});
+
+//라우터, 즉 get Method로 들어오는 것도 미들웨어 기반으로 구현된 것
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-//웹서버를 만든 것
-//listen() 서버를 이 포트로 키겠다
-//app.listen() -> 첫번째 인자 port, 두번째 인자 함수
-//port가 제대로 켜졌다면 두 번째에 있는 함수가 실행된다
 app.listen(port, () => {
   console.log(port, "포트로 서버가 켜졌어요");
 });
